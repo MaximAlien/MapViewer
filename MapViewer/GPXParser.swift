b3bacd5c8bbf1787c130b23e2bc505c4d079a553
Maxim Makhun <maxim.makhun@gmail.com>
2025-01-23 10:06:47 -0800

    Update.

--- MapViewer/GPXParser.swift
+++ MapViewer/GPXParser.swift
@@ -66,7 +66,7 @@ class GPXParser: NSObject, XMLParserDelegate {
     }
 }
 
-extension CLLocationCoordinate2D: Hashable {
+extension CLLocationCoordinate2D: @retroactive Hashable {
 
     public static func == (lhs: Self, rhs: Self) -> Bool {
         return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
